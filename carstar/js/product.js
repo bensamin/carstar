4dbca41b6fb565a3e435d718c7e8164a4276ea30
WEB <WEB@localhost>
2017-05-19 16:00:41 +0800

    购物车 少店铺商品选择

--- carstar/js/product.js
+++ carstar/js/product.js
@@ -1,74 +1,4 @@
- 
- $(document).ready(function(){
- })
- 
- //商品数量加减
-$(".subNum").click(function(){
-	var t = $(this).parent().find("input[class*=text-box]");
-	var p = 0;   //单个商品总价
-	t.val( parseInt(t.val() )-1 );
-	
-	if( parseInt(t.val()) < 2){
-			t.val(1);
-		}
-	
-		p = t.val() * parseFloat( $(this).parent().prev().find( "span[class*=text-price]" ).text() ) ;
-		$( $(this).parent().next().find("span[class*=product-total]") ).html(p.toFixed(2));
-		setPriceTotal();
-		setNumTotal();
-});
 
-$(".addNum").click(function(){
-	var t = $(this).parent().find("input[class*=text-box]");		
-	var p = 0;
-	var n = $('.productnumber').text();
-	
-	t.val(parseInt(t.val()) + 1) ;
-	
-	if( parseInt(t.val()) > n ){
-		t.val(n);
-	}
-	
-	p = t.val() * parseFloat( $(this).parent().prev().find( "span[class*=text-price]" ).text() ) ;
-	$( $(this).parent().next().find("span[class*=product-total]") ).html(p.toFixed(2));
-	setPriceTotal();
-	setNumTotal();
-});
-//-----------------END
-//计算总数量
-function setNumTotal(){
-	var n  = 0;
-	$(".text-box").each(function(){
-		
-		n += parseInt( $(this).val()  );
-//		console.log( n );
-	});
-	$(".product-num").html(n);
-}
-//计算总价格
-function setPriceTotal(){
-	var p = 0;
-	$(".product1").each(function(){
-		p += parseInt( $(this).find("input[class*=text-box]").val() ) * parseFloat( $(this).find("span[class*=text-price]").text());	
-			});
-		$(".total-all").html( p.toFixed(2) );
-	};
-//页面加载完毕执行函数
-$(document).ready(function(){
-			selectAll();
-			setPriceTotal();
-			setNumTotal();
-			
-			var p = 0;
-			$(".product1").each(function(){
-//				p = parseFloat( $(this).find("span[class*=text-price]").text() );
-			p = parseInt( $(this).find("input[class*=text-box]").val() ) * parseFloat( $(this).find("span[class*=text-price]").text());	
-			$(this).find("span[class*=product-total]").html( p.toFixed(2) );
-//				p += parseInt( $(this).find("input[class*=text-box]").val() ) * parseFloat( $(this).find("span[class*=text-price]").text());
-			}); 
-			$(".total-all").html( p.toFixed(2) );
-		});
-//-----------END
  
 $(".btnno").click(function(){
 	$(".discount-no").slideToggle();
@@ -103,12 +33,17 @@ function selectAll(){
 			
 		};
 	
+function XXURL(){
 	var thisURL =  document.URL;
 	var getVal = thisURL.split('?')[1];
 	var pid = getVal.split("=")[1];	
+	return pid;
+}
+
 //产品详情 接受产品id和商品信息
 function viewProduct(){
 //	console.log(pid);
+	var pid = XXURL();
 	var p_url1 = "http://139.224.133.119:8080/CarStar/rest/goods/querygoods?goodsid="+pid;
 	$.ajax({
 		type:"get",
@@ -127,6 +62,7 @@ function viewProduct(){
 }
 
 function detailProduct(){
+	var pid = XXURL();
 	var p_url2 = "http://139.224.133.119:8080/CarStar/rest/property/getprop?goodsid="+pid;
 	$.ajax({
 		type:"get",
@@ -219,15 +155,12 @@ function insertProductInfo2(data){
 	
 }
 
-function pullPdata(){
-//	var a_url = "http://139.224.133.119:8080/CarStar/rest/property/getprinum?goodsid="+pid+"&property="+v[0]+","+v[1];
-}
-
 var defaultstats = true;
 var defaultstats2 = false;
 var v=new Array();
 var v1=new Array();
 function getTotalprice(data){
+	var pid = XXURL();
 	defaultstats2 = true;
 	$('.iteminfo_parameter').each(function(){
 		var i = $(this);
@@ -251,11 +184,11 @@ function getTotalprice(data){
 				if( msg.data == null ){
 					document.getElementsByClassName("productPriceFont").item(0).innerHTML = "0";
 					document.getElementsByClassName("productnumber").item(0).innerHTML = "0";
-					document.getElementsByClassName("text-box").item(0).value= "0";
+					document.getElementsByClassName("text1-box").item(0).value= "0";
 				}else{
 					document.getElementsByClassName("productPriceFont").item(0).innerHTML = msg.data[0].pprice;
 					document.getElementsByClassName("productnumber").item(0).innerHTML = msg.data[0].pnumber;
-					document.getElementsByClassName("text-box").item(0).value= "1";
+					document.getElementsByClassName("text1-box").item(0).value= "1";
 					v=[]
 				}
 			}
@@ -268,6 +201,7 @@ function getTotalprice(data){
 
 //加入购物车
 function addShopCart1(){
+	var pid = XXURL();
 	var userid = $.cookie("userId");
 	var num = $('#prodctNum').val();
 	if( !defaultstats2 ){
@@ -304,6 +238,7 @@ function getJsonLength(JsonData){
 
 //商品评价
 function commentProduct(){
+	var pid = XXURL();
 	var a_url = "http://139.224.133.119:8080/CarStar/rest/goodsrev/revinfo?goodsid="+pid+"&startNum=0&pageSize=5";
 	$.ajax({
 		type:"get",
@@ -369,26 +304,230 @@ function listShopCart(){
 		url:l_url,
 		async:true,
 		success:function(msg){
-			console.log(msg);
+//			console.log(msg);
 			addShopCart(msg.data);
 		}
 	});
 }
 
 function addShopCart(data){
-	console.log(data);
+//	console.log(data);
 	$.each(data, function(i) {
 		//遍历店铺
 			$('.shopCart').append(
 				  "<div class='row' style='border-bottom:1px solid #E3E3E3;padding-bottom:10px;'> " +
-				  "<div class='col-md-1 shopName3'> <input type='checkbox' /> </div>"+
+				  "<div class='col-md-1 shopName3'> <input type='checkbox'  name='cartCheckBox'/> </div>"+
 				 	 " <div class='col-md-1 shopCartStoreImg'><img class='' src=' "+ data[i].shopimg+" ' width='70' height='40'> </div>"+  
-				  "	<div class='col-md-1 shopName3'> <span> "+data[i].shopname+"</span> </div> </div>"
+				  "	<div class='col-md-3 shopName3'> 店铺：<span> "+data[i].shopname+"</span> </div> </div>"
 			)
+//			console.log(data[i].goods);
+			var pgoods = data[i].goods;
 		//遍历商品	
-		$.each(data.goods,function(i){
-			
+		$.each(pgoods,function(i){
+//			if ( pgoods[i].property == undefined ){ pgoods[i].property=''; }
+			$('.shopCart').append(
+				"<div class='row products' id='product"+i+"' data-pid='"+pgoods[i].orderid+"' >" +
+					"<div class='col-md-4 col-xs-4'>" +
+						"<input type='checkbox' name='cartCheckBox'  />"+
+						"<div class='row'>"+
+							"<div class='col-md-1'></div>"+
+							"<div class='col-md-4'>"+
+								"<img src='img/small-product.png'>"+
+							"</div>"+
+							"<div class='col-md-7'>"+
+								"<p>"+pgoods[i].goodsname+"</p>"+
+								"<p>"+pgoods[i].goodsdes+"</p>"+
+							"</div>"+
+						"</div>"+
+					"</div>"+
+					"<div class='col-md-2 col-xs-1'>"+
+						"<span>"+pgoods[i].property+"</span>"+
+					"</div>"+
+					"<div class='col-md-1 col-xs-2'>"+
+						"<span>¥</span>"+
+						"<span class='text-price'>"+pgoods[i].price+"</span>"+
+					"</div>"+
+					"<div class='col-md-2 col-xs-2'>"+
+						"<i class='disBlue subNum' onclick='changesumPnum(Pnum"+i+")'>-</i>"+
+						"<input type='text' id=Pnum"+i+" class='text-box' value='"+pgoods[i].num+"' />"+
+						"<i class='disBlue  addNum' onclick='changeaddPnum(Pnum"+i+")'>+</i>"+
+					"</div>"+
+					"<div class='col-md-1 col-xs-2'>"+
+						"<span style='color: red;'>¥</span> <span class='product-total' style='color: red;'></span>"+ //金额
+					"</div>"+
+					"<div class='col-md-1 col-xs-1'>"+
+						"<a href='javascript:deleteCartp(product"+i+");'>删除</a>"+
+					"</div>"+
+				"</div>"	
+			)
 		});
 	});
+	testReady();
+}
+
+//商品详情数量加减
+function sumNum(){
+	var t =$('.text1-box');
+	var p = 0;   //单个商品总价
+	t.val( parseInt(t.val() )-1 );
 	
+	if( parseInt(t.val()) < 2){
+			t.val(1);
+		}
+}
+
+function addNum() {
+	var t = $('.text1-box');	
+	var n = $('.productnumber').text();
+	t.val(parseInt(t.val()) + 1) ;
+	if( parseInt(t.val()) > n ){
+		t.val(n);
+	}
+}
+
+//购物车更新数量
+function changesumPnum(PnumId){
+	var pnumId = $(PnumId);
+	var cartId = pnumId.parent().parent().attr("data-pid");
+	var newNum;
+		if( pnumId.val() <=1 ){
+			alert(" 商品数量必须大于0！")
+			return false;
+		}else{
+			newNum= parseInt(pnumId.val() )-1
+			pnumId.val( newNum );
+			
+			//更新到后台
+			$.ajax({
+				type:"get",
+				url:"http://139.224.133.119:8080/CarStar/rest/goodscart/modifynum?cartid="+cartId+"&num="+newNum,
+				async:true,
+				success:function(msg){
+				}
+			});
+		}
+		//价格更新
+		var priceId = pnumId.parent().prev().find('.text-price').text();
+		var totalId = pnumId.parent().next().find('.product-total');
+		totalId.text( priceId * newNum );
+		setNumTotal();
+		setPriceTotal();
+}
+
+function changeaddPnum(PnumId){
+	var pnumId = $(PnumId);
+	var cartId = pnumId.parent().parent().attr("data-pid");
+	var newNum = parseInt(pnumId.val() )+1;
+	pnumId.val( newNum );
+	
+	//价格更新
+		var priceId = pnumId.parent().prev().find('.text-price').text();
+		var totalId = pnumId.parent().next().find('.product-total');
+		totalId.text( priceId * newNum );
+		
+			$.ajax({
+				type:"get",
+				url:"http://139.224.133.119:8080/CarStar/rest/goodscart/modifynum?cartid="+cartId+"&num="+newNum,
+				async:true,
+				success:function(msg){
+				}
+			});
+	setNumTotal();
+	setPriceTotal();		
+}
+
+function setNumTotal(){
+	var n  = 0;
+	$(".text-box").each(function(){
+		n += parseInt( $(this).val()  );
+	});
+	$(".product-num").html(n);
+}
+
+function setPriceTotal(){
+			var p = 0;
+			var p_all = 0;
+			$(".products").each(function(){
+				p =  parseInt( $(this).find("input[class*=text-box]").val() ) * parseFloat( $(this).find("span[class*=text-price]").text());	
+				$(this).find("span[class*=product-total]").html( p.toFixed(2) );
+//				console.log(p);
+				p_all += p;
+			}); 
+			$(".total-all").html( p_all.toFixed(2) );	
+}
+//页面加载完毕计算价格
+function testReady(){
+			changesumPnum();   //计算数量
+			changeaddPnum();
+			setPriceTotal();  //
+			setNumTotal();
+}
+//-----------END
+
+
+/*复选框全选或全不选效果*/
+function selectAll(){
+	var oInput=document.getElementsByName("cartCheckBox");
+	 for (var i=0;i<oInput.length;i++){
+ 	    oInput[i].checked=document.getElementById("SlectAllCbx1").checked;
+	}
+}
+
+//单个删除购物车商品
+function  deleteCartp(productid){
+	var Index=$(productid);
+	var cartid = Index.attr("data-pid");
+	Index.remove();    //删除物品所在的div
+//	$(".poducts").deleteRow(Index-1);
+		$.ajax({
+			type:"get",
+			url:"http://192.168.1.106:8080/CarStar/rest/goodscart/removecart?cartids="+cartid,
+			async:true,
+			success:function(msg){
+				console.log(msg);
+			}
+		});
+	testReady();
+}
+
+//批量删除
+function deletesCrtp(){
+	console.log("1");
+	var oInput=document.getElementsByName("cartCheckBox");
+	var cartids = [];
+	$(":checked").each(function(){
+		var m = $(this).parent().parent().attr("data-pid");
+		$(this).parent().parent().remove();
+		cartids.push(m);
+	});
+	console.log(cartids,cartids[0],cartids.length);
+	
+	//批量删除
+	var push_url = "http://139.224.133.119:8080/CarStar/rest/goodscart/removecart?cartids="+cartids;
+	$.ajax({
+		type:"get",
+		url:push_url,
+		async:true,
+		success:function(msg){
+			console.log(msg);
+		}
+	});
+	testReady();
+}
+
+//购物车搜索
+function searchShopcart(){
+	var text = $('.search-shopcart_input').val();
+	var userid = $.cookie("userId");
+	
+	$.ajax({
+		type:"get",
+		url:"http://139.224.133.119:8080/CarStar/rest/goodscart/findcart?userid="+userid+"&goodsname="+text,
+		async:true,
+		success:function(msg){
+			console.log(msg);
+			$('.shopCart').empty();
+			addShopCart(msg.data);
+		}
+	});
 }
